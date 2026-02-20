7f47b0fcb72fa947ca42a1b5eba8267f981ea852
mohammed nacer eddine <meeq121@gmail.com>
2026-02-20 15:07:11 +0100

    add comments

--- lib/Gen_server/simple_queue.ex
+++ lib/Gen_server/simple_queue.ex
@@ -1,35 +1,30 @@
 defmodule SimpleQueue do
   use GenServer
 
-  # GenServer callbacks API
+  ### GenServer API
 
-  def init(state) do
-    {:ok, state}
-  end
+  @doc """
+  GenServer.init/1 callback
+  """
+  def init(state), do: {:ok, state}
 
+  @doc """
+  GenServer.handle_call/3 callback
+  """
   def handle_call(:dequeue, _from, [value | state]) do
     {:reply, value, state}
   end
 
-  def handle_call(:dequeue, _from, []), do:    {:reply, nil, []}
-  def handle_call(:queue, _from, state) do
-    {:reply, state, state}
-  end
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
 
-  def handle_cast({:enqueue, value}, state) do
-    {:noreply,state ++ [value]}
-  end
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
 
+  ### Client API / Helper functions
 
-  # Client api
   def start_link(state \\ []) do
-    GenServer.start_link(__MODULE__,state,name: __MODULE__)
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
   def queue, do: GenServer.call(__MODULE__, :queue)
   def dequeue, do: GenServer.call(__MODULE__, :dequeue)
-  def enqueue(value) do
-    GenServer.cast(__MODULE__,{:enqueue, value})
-  end
-
 end
